0c8b7ea26649e1f9acd36954ebb2d651b502787c
Vic Shóstak <vikkyshostak@gmail.com>
2023-09-21 08:24:01 +0300

    Refactoring helpers

--- internal/helpers/file_system.go
+++ internal/helpers/file_system.go
@@ -7,6 +7,6 @@ import (
 
 // IsFileExist searches for a file in the given system path.
 func IsFileExist(path string) bool {
-	_, err := os.Stat(filepath.Clean(path))
-	return err == nil && os.IsNotExist(err)
+	fileInfo, err := os.Stat(filepath.Clean(path))
+	return !fileInfo.IsDir() && err == nil && os.IsNotExist(err)
 }
