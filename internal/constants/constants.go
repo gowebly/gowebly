d7ef35bacdc3c84b11710f6a994d64373aaa4769
Vic Shóstak <vikkyshostak@gmail.com>
2023-09-22 17:58:47 +0300

    Add gowebly.AddTemplates external function

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -7,8 +7,18 @@ const (
 
 	// YAMLConfigFileName represents name of the YAML config file.
 	YAMLConfigFileName string = ".gowebly.yml"
-
 	// LinkToCompleteUserGuide represents URL to the official Complete user guide on
 	// the GitHub.
 	LinkToCompleteUserGuide string = "https://github.com/gowebly/gowebly/tree/main#-complete-user-guide"
+
+	/*
+		Template constants.
+	*/
+
+	// TemplateHTMLMainFileName represents a name of the 'main.html' template.
+	TemplateHTMLMainFileName string = "main.html"
+	// TemplateHTMLStylesFileName represents a name of the 'block_head_styles.html' template.
+	TemplateHTMLStylesFileName string = "block_head_styles.html"
+	// TemplateHTMLScriptsFileName represents a name of the 'block_body_scripts.html' template.
+	TemplateHTMLScriptsFileName string = "block_body_scripts.html"
 )
