3a0474315396df52e8f2a62dfc404b674cab8099
Vic Shóstak <vikkyshostak@gmail.com>
2024-07-20 10:03:44 +0300

    Fix select order

--- internal/fields/select.go
+++ internal/fields/select.go
@@ -1,59 +1,55 @@
 package fields
 
 import (
+	"sort"
+
 	"github.com/charmbracelet/huh"
 	"github.com/gowebly/gowebly/v2/internal/injectors"
 	"github.com/gowebly/gowebly/v2/internal/messages"
 	"github.com/gowebly/gowebly/v2/internal/variables"
 )
 
-// GoFrameworkSelect runs the Go framework select.
-func GoFrameworkSelect(di *injectors.Injector) *huh.Select[string] {
+// sortMapByKey sorts the map by the keys.
+func sortMapByKey(inputs map[string][]string) []huh.Option[string] {
 	// Define options for the select.
-	options := make([]huh.Option[string], 0, len(variables.ListGoFrameworks))
+	options := make([]huh.Option[string], 0, len(inputs))
 
 	// Add options to the select.
-	for _, framework := range variables.ListGoFrameworks {
-		options = append(options, huh.NewOption(framework[1], framework[0]))
+	for _, input := range inputs {
+		options = append(options, huh.NewOption(input[1], input[0]))
 	}
 
+	// Sort the options slice by the keys.
+	sort.Slice(options, func(i, j int) bool {
+		return options[i].Key < options[j].Key
+	})
+
+	return options
+}
+
+// GoFrameworkSelect runs the Go framework select.
+func GoFrameworkSelect(di *injectors.Injector) *huh.Select[string] {
 	return huh.NewSelect[string]().
 		Title(messages.FormGoFrameworkTitle).
 		Description(messages.FormGoFrameworkDescription).
-		Options(options...).
+		Options(sortMapByKey(variables.ListGoFrameworks)...).
 		Value(&di.Config.Backend.GoFramework)
 }
 
 // ReactivityLibrarySelect runs the reactivity library select.
 func ReactivityLibrarySelect(di *injectors.Injector) *huh.Select[string] {
-	// Define options for the select.
-	options := make([]huh.Option[string], 0, len(variables.ListReactivityLibraries))
-
-	// Add options to the select.
-	for _, library := range variables.ListReactivityLibraries {
-		options = append(options, huh.NewOption(library[1], library[0]))
-	}
-
 	return huh.NewSelect[string]().
 		Title(messages.FormReactivityLibraryTitle).
 		Description(messages.FormReactivityLibraryDescription).
-		Options(options...).
+		Options(sortMapByKey(variables.ListReactivityLibraries)...).
 		Value(&di.Config.Frontend.ReactivityLibrary)
 }
 
 // CSSFrameworkSelect runs the CSS framework select.
 func CSSFrameworkSelect(di *injectors.Injector) *huh.Select[string] {
-	// Define options for the select.
-	options := make([]huh.Option[string], 0, len(variables.ListCSSFrameworks))
-
-	// Add options to the select.
-	for _, framework := range variables.ListCSSFrameworks {
-		options = append(options, huh.NewOption(framework[1], framework[0]))
-	}
-
 	return huh.NewSelect[string]().
 		Title(messages.FormCSSFrameworkTitle).
 		Description(messages.FormCSSFrameworkDescription).
-		Options(options...).
+		Options(sortMapByKey(variables.ListCSSFrameworks)...).
 		Value(&di.Config.Frontend.CSSFramework)
 }
